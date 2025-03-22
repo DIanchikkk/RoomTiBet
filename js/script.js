359484ff2bf7f54ec0cad7feefe28ecddef0c899
Diana <diana_yusupovaa@mail.ru>
2025-03-23 02:28:38 +0300

    ..

--- js/script.js
+++ js/script.js
@@ -1,2 +1,7 @@
 Fancybox.bind("[data-fancybox]", {});
 
+document.querySelector('.header__burger').addEventListener('click', function() {
+    this.classList.toggle('active');
+    console.log("Кнопка .header__burger: класс active теперь:", this.classList.contains('active')); // Добавлено console.log
+    document.querySelector('.header__right').classList.toggle('open');
+});
